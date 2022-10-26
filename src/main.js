8507232974e7e987afce711bfe331a95c152be65
Eirik Mo <eirik@mowebdev.com>
2022-10-26 11:14:43 +0200

    Add vue-router and routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+import MainDashboard from '@/components/MainDashboard'
+import NewEmployee from '@/components/NewEmployee'
+import EditEmployee from '@/components/EditEmployee'
+import ViewEmployee from '@/components/ViewEmployee'
+
+const routes = [
+  { path: '/', component: MainDashboard },
+  { path: '/new', component: NewEmployee },
+  {
+    path: '/edit/:employee_id',
+    component: EditEmployee,
+  },
+  {
+    path: '/:employee_id',
+    component: ViewEmployee,
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
